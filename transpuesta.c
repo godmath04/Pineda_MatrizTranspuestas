ca1b7301e3ae6538d2c30d8d5670da60c166baf1
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2023-11-27 15:51:54 -0500

    Matriz llena de numeros aleatorios

---
+++ transpuesta.c
@@ -0,0 +1,36 @@
+/*
+Usuario ingrese las dimensiones de un arreglo bidimensional (matriz),
+el programa llene la matriz con números aleatorios entre 0 y 100.
+ Luego, calcule la matriz transpuesta. El programa debe imprimir la matriz original y la matriz transpuesta.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char const *argv[])
+{
+    srand(time(NULL));
+    // Tamaño de la matriz
+     int fila = 0, columna = 0;
+    printf("Ingrese las dimensiones para su matriz. \n");
+    printf("Ingrese el numero de fila: \n");
+    scanf("%d", &fila);
+    printf("Ingrese la dimension de su columna: \n");
+    scanf("%d", &columna);
+
+    // Matriz llena numeros aletorios
+    int Matriz[fila][columna];
+    for (int i = 0; i < fila; i++)
+    {
+        for (int j = 0; j < columna; j++)
+        {
+            // A cada elemento de la matriz se asigna el cero
+            Matriz[i][j] = rand() %101 ;
+            // Aprovechar el bucle para la impresión
+            printf("%4d |", Matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
